63980bd463b5481f26d4a854fc584d07d3c6169b
Andrew Walter <awalter@bnl.gov>
2024-08-15 13:59:52 -0400

    initial setup files

---
+++ docs/index.md
@@ -0,0 +1,17 @@
+# test_data
+
+```{toctree}
+:maxdepth: 2
+:hidden:
+
+```
+
+```{include} ../README.md
+:start-after: <!-- SPHINX-START -->
+```
+
+## Indices and tables
+
+- {ref}`genindex`
+- {ref}`modindex`
+- {ref}`search`
---
+++ src/test_data/__init__.py
@@ -0,0 +1,11 @@
+"""
+Copyright (c) 2024 Andrew Walter. All rights reserved.
+
+test_data: Test data to be used for data processing, data analysis and simulation code at the ARI beamline
+"""
+
+from __future__ import annotations
+
+from ._version import version as __version__
+
+__all__ = ["__version__"]
